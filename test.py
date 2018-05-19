d5797b294114d6a29d61b697d41e00683a936880
Florian Schellenberg <florian.schellenberg@gmail.com>
2018-05-19 10:01:29 +0200

    Sync

--- test.py
+++ test.py
@@ -1,24 +1,33 @@
-print("hello")
-# Fibonacci numbers module
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
 
-def fib(n):    # write Fibonacci series up to n
-    a, b = 0, 1
-    while b < n:
-        print(b, end=' ')
-        a, b = b, a+b
-    print()
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
 
-def fib2(n):   # return Fibonacci series up to n
-    result = []
-    a, b = 0, 1
-    while b < n:
-        result.append(b)
-        a, b = b, a+b
-    return result
 
-# comment
+# print("hello")
+# # Fibonacci numbers module
 
-import pygame
-pygame.init()
+# def fib(n):    # write Fibonacci series up to n
+#     a, b = 0, 1
+#     while b < n:
+#         print(b, end=' ')
+#         a, b = b, a+b
+#     print()
 
-drum = pygame.mixer.sound("samples/drum_tom_mid_hard.wav")
+# def fib2(n):   # return Fibonacci series up to n
+#     result = []
+#     a, b = 0, 1
+#     while b < n:
+#         result.append(b)
+#         a, b = b, a+b
+#     return result
+
+# # comment
+
+# import pygame
+# pygame.init()
+
+# drum = pygame.mixer.sound("samples/drum_tom_mid_hard.wav")
